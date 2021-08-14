8182d0104357b1f10fc07173c9b4ef48ada8e8f1
Blake <btdubbzzz@gmail.com>
2021-08-14 14:08:36 -0500

    one more commit before class ends

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -6,14 +6,12 @@ import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
-import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import { Modal } from '@material-ui/core';
-import { useState } from 'react';
-import SignUp from '../components/SignUp';
+import Container from '@material-ui/core/Container';
 
 function Copyright() {
 	return (
@@ -29,11 +27,8 @@ function Copyright() {
 }
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		height: '100vh',
-	},
 	paper: {
-		margin: theme.spacing(8, 4),
+		marginTop: theme.spacing(8),
 		display: 'flex',
 		flexDirection: 'column',
 		alignItems: 'center',
@@ -44,63 +39,81 @@ const useStyles = makeStyles((theme) => ({
 	},
 	form: {
 		width: '100%', // Fix IE 11 issue.
-		marginTop: theme.spacing(1),
+		marginTop: theme.spacing(3),
 	},
 	submit: {
 		margin: theme.spacing(3, 0, 2),
 	},
 }));
 
-export default function SignInSide() {
+export default function SignUp() {
 	const classes = useStyles();
-	const handleOpen = () => {
-		setOpen(true);
-	};
-
-	const handleClose = () => {
-		setOpen(false);
-	};
-
-	const [open, setOpen] = useState(false);
 
 	return (
-		<Grid container component="main" className={classes.root}>
+		<Container component="main" maxWidth="xs">
 			<CssBaseline />
-
 			<div className={classes.paper}>
 				<Avatar className={classes.avatar}>
 					<LockOutlinedIcon />
 				</Avatar>
 				<Typography component="h1" variant="h5">
-					Sign in
+					Sign up
 				</Typography>
 				<form className={classes.form} noValidate>
-					<TextField
-						variant="outlined"
-						margin="normal"
-						required
-						fullWidth
-						id="email"
-						label="Email Address"
-						name="email"
-						autoComplete="email"
-						autoFocus
-					/>
-					<TextField
-						variant="outlined"
-						margin="normal"
-						required
-						fullWidth
-						name="password"
-						label="Password"
-						type="password"
-						id="password"
-						autoComplete="current-password"
-					/>
-					<FormControlLabel
-						control={<Checkbox value="remember" color="primary" />}
-						label="Remember me"
-					/>
+					<Grid container spacing={2}>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								autoComplete="fname"
+								name="firstName"
+								variant="outlined"
+								required
+								fullWidth
+								id="firstName"
+								label="First Name"
+								autoFocus
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								variant="outlined"
+								required
+								fullWidth
+								id="lastName"
+								label="Last Name"
+								name="lastName"
+								autoComplete="lname"
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant="outlined"
+								required
+								fullWidth
+								id="email"
+								label="Email Address"
+								name="email"
+								autoComplete="email"
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant="outlined"
+								required
+								fullWidth
+								name="password"
+								label="Password"
+								type="password"
+								id="password"
+								autoComplete="current-password"
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<FormControlLabel
+								control={<Checkbox value="allowExtraEmails" color="primary" />}
+								label="I want to receive inspiration, marketing promotions and updates via email."
+							/>
+						</Grid>
+					</Grid>
 					<Button
 						type="submit"
 						fullWidth
@@ -108,34 +121,20 @@ export default function SignInSide() {
 						color="primary"
 						className={classes.submit}
 					>
-						Sign In
+						Sign Up
 					</Button>
-					<Grid container>
-						<Grid item xs>
+					<Grid container justifyContent="flex-end">
+						<Grid item>
 							<Link href="#" variant="body2">
-								Forgot password?
+								Already have an account? Sign in
 							</Link>
 						</Grid>
-						<Grid item>
-							<button type="button" onClick={handleOpen}>
-								Open Modal
-							</button>
-							<Modal
-								open={open}
-								onClose={handleClose}
-								aria-labelledby="simple-modal-title"
-								aria-describedby="simple-modal-description"
-								BackdropComponent={classes.submit}
-							>
-								<SignUp></SignUp>
-							</Modal>
-						</Grid>
 					</Grid>
-					<Box mt={5}>
-						<Copyright />
-					</Box>
 				</form>
 			</div>
-		</Grid>
+			<Box mt={5}>
+				<Copyright />
+			</Box>
+		</Container>
 	);
 }
