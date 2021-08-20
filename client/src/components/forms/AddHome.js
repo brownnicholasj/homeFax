a4b9070b00f0e264d34eb5057d5a51db98da4939
Edward Reed <relentlessreed@gmail.com>
2021-08-20 09:16:47 -0500

    cleaned up AddHome.js imports

--- client/src/components/forms/AddHome.js
+++ client/src/components/forms/AddHome.js
@@ -5,16 +5,10 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import { Divider } from '@material-ui/core';
-
 import TextField from '@material-ui/core/TextField';
 import { useMutation } from '@apollo/client';
-import {
-    ADD_HOME,
-    TRANSFER_HOME
- } from '../../utils/mutations';
-
+import { ADD_HOME, TRANSFER_HOME } from '../../utils/mutations';
 import Snack from '../Snack';
-
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -28,8 +22,8 @@ const useStyles = makeStyles((theme) => ({
     },
     inputRoot: {
         '& .MuiTextField-root': {
-          margin: theme.spacing(1),
-          width: '25ch',
+            margin: theme.spacing(1),
+            width: '25ch',
         },
     },
     gridRoot: {
@@ -42,9 +36,9 @@ export default function AddHome({ userId }) {
     const classes = useStyles();
     const [snack, setSnack] = useState({ status: false, message: '' });
     const [formState, setFormState] = useState({ street1: '', street2: '', city: '', state: '', zip: '' });
-	const [addHome, { addError }] = useMutation(ADD_HOME);
-	const [assignHome, { assignError }] = useMutation(TRANSFER_HOME);
-	const handleFormSubmit = async (event) => {
+    const [addHome, { addError }] = useMutation(ADD_HOME);
+    const [assignHome, { assignError }] = useMutation(TRANSFER_HOME);
+    const handleFormSubmit = async (event) => {
         event.preventDefault();
         if (userId && formState.street1) {
             try {
@@ -67,89 +61,89 @@ export default function AddHome({ userId }) {
                 console.log(e);
             }
         }
-	};
+    };
 
-	const handleChange = (event) => {
-		const { id, value } = event.target;
-		setFormState({
-			...formState,
-			[id]: value,
-		});
-	};
+    const handleChange = (event) => {
+        const { id, value } = event.target;
+        setFormState({
+            ...formState,
+            [id]: value,
+        });
+    };
 
 
-  return (
-    <>
-        <Card className={classes.root} variant="outlined">
-            <CardContent>
-                <div className={classes.gridRoot}>
-                    <Grid container spacing={1}>
-                        <Grid item xs={12}>
-                            <h3>Add home</h3>
-                            <Divider />
-                        </Grid>
-                        <Grid item xs={12} s={6}>
-                            <form className={classes.inputRoot} noValidate autoComplete="off">
-                                <div>
-                                <TextField
-                                    required
-                                    id="street1"
-                                    label="Street(1)"
-                                    variant="standard"
-                                    onChange={handleChange}
-                                />
-                                <TextField
-                                    id="street2"
-                                    label="Street(2)"
-                                    variant="standard"
-                                    onChange={handleChange}
-                                />
-                                <TextField
-                                    required
-                                    id="city"
-                                    label="City"
-                                    variant="standard"
-                                    onChange={handleChange}
-                                />
-                                <TextField
-                                    required
-                                    id="state"
-                                    label="State"
-                                    variant="standard"
-                                    onChange={handleChange}
-                                />
-                                <TextField
-                                    required
-                                    id="zip"
-                                    label="Zip"
-                                    variant="standard"
-                                    onChange={handleChange}
-                                />
-                                </div>
-                                <Button
-                                    color="primary"
-                                    variant="outlined"
-                                    size="large"
-                                    type="submit"
-                                    onClick={handleFormSubmit}>
+    return (
+        <>
+            <Card className={classes.root} variant="outlined">
+                <CardContent>
+                    <div className={classes.gridRoot}>
+                        <Grid container spacing={1}>
+                            <Grid item xs={12}>
+                                <h3>Add home</h3>
+                                <Divider />
+                            </Grid>
+                            <Grid item xs={12} s={6}>
+                                <form className={classes.inputRoot} noValidate autoComplete="off">
+                                    <div>
+                                        <TextField
+                                            required
+                                            id="street1"
+                                            label="Street(1)"
+                                            variant="standard"
+                                            onChange={handleChange}
+                                        />
+                                        <TextField
+                                            id="street2"
+                                            label="Street(2)"
+                                            variant="standard"
+                                            onChange={handleChange}
+                                        />
+                                        <TextField
+                                            required
+                                            id="city"
+                                            label="City"
+                                            variant="standard"
+                                            onChange={handleChange}
+                                        />
+                                        <TextField
+                                            required
+                                            id="state"
+                                            label="State"
+                                            variant="standard"
+                                            onChange={handleChange}
+                                        />
+                                        <TextField
+                                            required
+                                            id="zip"
+                                            label="Zip"
+                                            variant="standard"
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                    <Button
+                                        color="primary"
+                                        variant="outlined"
+                                        size="large"
+                                        type="submit"
+                                        onClick={handleFormSubmit}>
                                         Save Home
-                                </Button>
-                            </form>
-                            {snack.status ? (
+                                    </Button>
+                                </form>
+                                {snack.status ? (
                                     <Snack
-                                    setOpen={setSnack}
-                                    status={snack.status}
-                                    message={snack.message}
+                                        setOpen={setSnack}
+                                        status={snack.status}
+                                        message={snack.message}
                                     />
                                 ) : (
                                     null
                                 )}
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </div>
-            </CardContent>
-        </Card>
-    </>
-  );
+                    </div>
+                </CardContent>
+            </Card>
+        </>
+    );
 }
 
