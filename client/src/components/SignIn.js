15858d2de7d7fa82809c64d26909d27625897290
Nicholas Brown <brown.nicholasj@gmail.com>
2021-08-20 06:13:20 -0500

    updated login component to remove forgot password button without functionality

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -107,7 +107,7 @@ export default function SignIn() {
 			const { user, token } = mutationResponse.data.login;
 			dispatch({ type: UPDATE_USER, user });
 			const homes = user.homes;
-			dispatch({ type: UPDATE_HOMES, homes })
+			dispatch({ type: UPDATE_HOMES, homes });
 
 			Auth.login(token);
 		} catch (e) {
@@ -189,12 +189,7 @@ export default function SignIn() {
 					>
 						Sign In
 					</Button>
-					<Grid container>
-						<Grid item xs>
-							<Link href='#' variant='body2'>
-								Forgot password?
-							</Link>
-						</Grid>
+					<Grid container justifyContent='center'>
 						<Grid item>
 							<Link href='#' variant='body2' onClick={handleOpen}>
 								Or Sign Up
