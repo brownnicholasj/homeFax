89275df7035fe6ef0bf7a322a64512fd48cb8ff3
brownnicholasj <brown.nicholasj@gmail.com>
2021-08-22 20:30:41 -0500

    getting counter badge to work

--- server/models/Transfer.js
+++ server/models/Transfer.js
@@ -3,25 +3,20 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const transferSchema = new Schema({
-  transferer: [
-    {
-      type: String,
-      trim: true
-    }
-  ],
-  receiver: [
-    {
-      type: String,
-      trim: true
-    }
-  ],
-  home: {
-    type: String,
-    required: true,
-    unique: true
-  },
+	transferer: {
+		type: String,
+		trim: true,
+	},
+	receiver: {
+		type: String,
+		trim: true,
+	},
+	home: {
+		type: String,
+		required: true,
+		unique: true,
+	},
 });
-
 
 const Transfer = mongoose.model('Transfer', transferSchema);
 
